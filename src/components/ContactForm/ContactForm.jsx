6e20d68468b47608ab6a2ed7e3bac31be3d49909
Lucas Schvindt <lucas.schvindt@gmail.com>
2023-08-05 15:12:58 -0400

    Added ContactForm component and Theme toggle

---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,201 @@
+import { useState } from 'react';
+import FormControl from '@mui/joy/FormControl';
+import FormLabel from '@mui/joy/FormLabel';
+import Input from '@mui/joy/Input';
+import Textarea from '@mui/joy/Textarea';
+import Button from '@mui/joy/Button';
+import Alert from '@mui/joy/Alert';
+import Modal from '@mui/joy/Modal';
+import ModalClose from '@mui/joy/ModalClose';
+import ModalDialog from '@mui/joy/ModalDialog';
+import Typography from '@mui/joy/Typography';
+
+import { validateEmail } from '../../utils/helpers';
+
+function ContactForm() {
+    const [name, setName] = useState('');
+    const [nameError, setNameError] = useState('');
+    const [email, setEmail] = useState('');
+    const [emailError, setEmailError] = useState('');
+    const [message, setMessage] = useState('');
+    const [messageError, setMessageError] = useState('');
+    const [open, setOpen] = useState(false);
+
+    const handleInputChange = (e) => {
+        const { target } = e;
+        const inputType = target.name;
+        const inputValue = target.value;
+    
+        if (inputType === 'email') {
+            setEmail(inputValue);
+            setEmailError('');
+        } else if (inputType === 'name') {
+            setName(inputValue);
+            setNameError('');
+        } else {
+            setMessage(inputValue);
+            setMessageError('');
+        }
+    };
+
+    const handleInputBlur = (e) => {
+        const { target } = e;
+        const inputType = target.name;
+        const inputValue = target.value;
+    
+        if (inputType === 'email' && inputValue === '') {
+            setEmailError('Email cannot be left empty');
+        } else if (inputType === 'name' && inputValue === '') {
+            setNameError('Name cannot be left empty');
+        } else if (inputValue === '') {
+            setMessageError('Message cannot be left empty');
+        } else if (!validateEmail(email)) {
+            setEmailError('Email is invalid');
+        }
+    };
+
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+
+        if (name === '') {
+            setNameError('Name cannot be left empty');
+        }
+
+        if (email === '') {
+            setEmailError('Email cannot be left empty');
+        } else if (!validateEmail(email)) {
+            setEmailError('Email is invalid');
+        }
+
+        if (message === '') {
+            setMessageError('Message cannot be left empty');
+        }
+
+        // If everything passes, submit the form and clear the input values
+        if (name !== '' && email !== '' && message !== '' && validateEmail(email)) {
+            setName('');
+            setEmail('');
+            setMessage('');
+            setNameError('');
+            setEmailError('');
+            setMessageError('');
+            setOpen(true);
+        }
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    return (
+        <>
+            <form className="form" onSubmit={handleFormSubmit}>
+                <FormControl>
+                    <FormLabel>Name</FormLabel>
+                    <Input
+                        name="name"
+                        value={name}
+                        onChange={handleInputChange}
+                        onBlur={handleInputBlur}
+                        type="text"
+                        placeholder="Name"
+                        error={!!nameError}
+                    />
+                    {nameError && (
+                        <Alert
+                            size="sm"
+                            variant="plain"
+                            color="danger"
+                        >
+                            {nameError}
+                        </Alert>
+                    )}
+                </FormControl>
+
+                <FormControl>
+                    <FormLabel>Email</FormLabel>
+                    <Input
+                        value={email}
+                        onChange={handleInputChange}
+                        onBlur={handleInputBlur}
+                        name="email"
+                        type="email"
+                        placeholder="johndoe@email.com"
+                        error={!!emailError}
+                    />
+                    {emailError && (
+                        <Alert
+                            size="sm"
+                            variant="plain"
+                            color="danger"
+                        >
+                            {emailError}
+                        </Alert>
+                    )}
+                </FormControl>
+
+                <FormControl>
+                    <FormLabel>Message</FormLabel>
+                    <Textarea
+                        value={message}
+                        onChange={handleInputChange}
+                        onBlur={handleInputBlur}
+                        minRows={2}
+                        placeholder="Your message..."
+                        error={!!messageError}
+                    />
+                    {messageError && (
+                        <Alert
+                            size="sm"
+                            variant="plain"
+                            color="danger"
+                        >
+                            {messageError}
+                        </Alert>
+                    )}
+                </FormControl>
+
+                <Button type="submit">Log in</Button>
+            </form>
+            <Modal
+                variant="plain"
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="modal-title"
+                aria-describedby="modal-desc"
+            >
+                <ModalDialog
+                    aria-labelledby="variant-modal-title"
+                    aria-describedby="variant-modal-description"
+                    variant="plain"
+                >
+                    <ModalClose
+                        variant="plain"
+                        sx={{
+                            top: 'calc(-1/4 * var(--IconButton-size))',
+                            right: 'calc(-1/4 * var(--IconButton-size))',
+                            boxShadow: '0 2px 12px 0 rgba(0 0 0 / 0.2)',
+                            borderRadius: '50%',
+                            bgcolor: 'background.surface',
+                        }}
+                    />
+                    <Typography
+                        component="h2"
+                        id="modal-title"
+                        level="h4"
+                        textColor="inherit"
+                        fontWeight="lg"
+                        mb={1}
+                    >
+                        Message sent!
+                    </Typography>
+                    <Typography id="modal-desc" textColor="text.tertiary">
+                        Well... kinda. This is just a demo. But soon it will!
+                    </Typography>
+                </ModalDialog>
+            </Modal>
+        </>
+    );
+}
+
+export default ContactForm;
---
+++ src/components/ModeToggle/ModeToggle.jsx
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import { useColorScheme } from '@mui/joy/styles';
+import Button from '@mui/joy/Button';
+
+function ModeToggle() {
+    const { mode, setMode } = useColorScheme();
+    const [mounted, setMounted] = React.useState(false);
+
+    // necessary for server-side rendering
+    // because mode is undefined on the server
+    React.useEffect(() => {
+        setMounted(true);
+    }, []);
+    if (!mounted) {
+        return null;
+    }
+
+    return (
+        <Button
+            variant="soft"
+            onClick={() => {
+                setMode(mode === 'light' ? 'dark' : 'light');
+            }}
+        >
+            {mode === 'light' ? 'Turn dark' : 'Turn light'}
+        </Button>
+    );
+}
+
+export default ModeToggle;
