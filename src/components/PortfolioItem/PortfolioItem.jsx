9bf4329e1f5efd69a440b15a030321da3745cb54
Lucas Schvindt <lucas.schvindt@gmail.com>
2023-08-08 20:11:37 -0400

    Adding portfolio images and code

--- src/components/PortfolioItem/PortfolioItem.jsx
+++ src/components/PortfolioItem/PortfolioItem.jsx
@@ -1,87 +1,161 @@
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 import AspectRatio from '@mui/joy/AspectRatio';
 import Box from '@mui/joy/Box';
 import Card from '@mui/joy/Card';
 import CardCover from '@mui/joy/CardCover';
 import Typography from '@mui/joy/Typography';
-import Link from '@mui/joy/Link';
-// import styles from './PortfilioItem.module.css';
-
-function PortfolioItem ({ title, image, link }) {
+import IconButton from '@mui/joy/IconButton';
+import LinkIcon from '../Icons/LinkIcon/LinkIcon';
+import GithubIcon from '../Icons/GithubIcon/GithubIcon';
+import Tooltip from '@mui/joy/Tooltip';
+import Modal from '@mui/joy/Modal';
+import ModalClose from '@mui/joy/ModalClose';
+function PortfolioItem ({ title, image, repoLink, webLink }) {
+    const [open, setOpen] = useState(false);
+    const handleOpen = () => {
+        console.log('Image clicked!'); // Check if this is being logged
+        setOpen(true);
+    };
+      
+    const handleClose = () => {
+        setOpen(false);
+    };
 
     return (
-        <Card
-            component="li"
-            sx={{
-                minWidth: 300,
-                flexGrow: 1,
-                bgcolor: 'initial',
-                boxShadow: 'none',
-                '--Card-padding': '0px',
-            }}
-        >
-            <Box sx={{ position: 'relative' }}>
-                <AspectRatio ratio="4/3">
-                    <figure>
-                        <img
-                        src={image}
-                        srcSet={`${image}&dpr=2 2x`}
-                        loading="lazy"
-                        alt={title}
-                        />
-                    </figure>
-                </AspectRatio>
-                <CardCover
-                    className="gradient-cover"
-                    sx={{
-                        '&:hover, &:focus-within': {
-                            opacity: 1,
-                        },
-                        opacity: 0,
-                        transition: '0.1s ease-in',
-                        background:
-                        'linear-gradient(180deg, transparent 62%, rgba(0,0,0,0.00345888) 63.94%, rgba(0,0,0,0.014204) 65.89%, rgba(0,0,0,0.0326639) 67.83%, rgba(0,0,0,0.0589645) 69.78%, rgba(0,0,0,0.0927099) 71.72%, rgba(0,0,0,0.132754) 73.67%, rgba(0,0,0,0.177076) 75.61%, rgba(0,0,0,0.222924) 77.56%, rgba(0,0,0,0.267246) 79.5%, rgba(0,0,0,0.30729) 81.44%, rgba(0,0,0,0.341035) 83.39%, rgba(0,0,0,0.367336) 85.33%, rgba(0,0,0,0.385796) 87.28%, rgba(0,0,0,0.396541) 89.22%, rgba(0,0,0,0.4) 91.17%)',
-                    }}
-                >
-                    {/* The first box acts as a container that inherits style from the CardCover */}
-                    <Box>
-                        <Box
-                            sx={{
-                                p: 2,
-                                display: 'flex',
-                                alignItems: 'center',
-                                gap: 1.5,
-                                flexGrow: 1,
-                                alignSelf: 'flex-end',
-                            }}
-                        >
-                            <Typography level="h2" noWrap sx={{ fontSize: 'lg' }}>
-                                <Link
-                                    href={link}
-                                    overlay
-                                    underline="none"
+        <>
+            <Card
+                component='li'
+                onClick={handleOpen}
+                sx={{
+                    minWidth: 300,
+                    flexGrow: 1,
+                    bgcolor: 'initial',
+                    boxShadow: 'none',
+                    '--Card-padding': '0px',
+                    cursor: 'pointer',
+                }}
+            >
+                <Box sx={{ position: 'relative' }}>
+                    <AspectRatio ratio='16/10'>
+                        <figure>
+                            <img
+                                src={image}
+                                loading='lazy'
+                                alt={title}
+                            />
+                        </figure>
+                    </AspectRatio>
+                    <CardCover
+                        className='gradient-cover'
+                        sx={{
+                            '&:hover, &:focus-within': {
+                                opacity: 1,
+                            },
+                            opacity: 0,
+                            transition: '0.1s ease-in',
+                            background:
+                            'linear-gradient(180deg, transparent 62%, rgba(0,0,0,0.00345888) 63.94%, rgba(0,0,0,0.014204) 65.89%, rgba(0,0,0,0.0326639) 67.83%, rgba(0,0,0,0.0589645) 69.78%, rgba(0,0,0,0.0927099) 71.72%, rgba(0,0,0,0.132754) 73.67%, rgba(0,0,0,0.177076) 75.61%, rgba(0,0,0,0.222924) 77.56%, rgba(0,0,0,0.267246) 79.5%, rgba(0,0,0,0.30729) 81.44%, rgba(0,0,0,0.341035) 83.39%, rgba(0,0,0,0.367336) 85.33%, rgba(0,0,0,0.385796) 87.28%, rgba(0,0,0,0.396541) 89.22%, rgba(0,0,0,0.4) 91.17%)',
+                        }}
+                    >
+                        {/* The first box acts as a container that inherits style from the CardCover */}
+                        <Box>
+                            <Box
+                                sx={{
+                                    p: 2,
+                                    display: 'flex',
+                                    alignItems: 'center',
+                                    gap: 1.5,
+                                    flexGrow: 1,
+                                    alignSelf: 'flex-end',
+                                }}
+                            >
+                                <Typography
+                                    level='h2'
+                                    noWrap
                                     sx={{
+                                        fontSize: 'lg',
                                         color: '#fff',
                                         textOverflow: 'ellipsis',
-                                        overflow: 'hidden',
+                                        overflow: 'visible',
                                         display: 'block',
+                                        position: 'relative',
+                                        width: '100%',
                                     }}
                                 >
-                                    {title}
-                                </Link>
-                            </Typography>
+                                        {title}
+                                    <Tooltip title='Repository' variant='solid'>
+                                        <IconButton
+                                            onClick={() => window.open(repoLink, '_blank')}
+                                            sx={{
+                                                position: 'absolute',
+                                                bottom: '0',
+                                                right: '0',
+                                            }}
+                                        >
+                                            <GithubIcon fillColor='#FFF' />
+                                        </IconButton>
+                                    </Tooltip>
+                                    <Tooltip title='Website' variant='solid'>
+                                        <IconButton
+                                            onClick={() => window.open(webLink, '_blank')}
+                                            sx={{
+                                                position: 'absolute',
+                                                bottom: '0',
+                                                right: '46px',
+                                            }}
+                                        >
+                                            <LinkIcon />
+                                        </IconButton>
+                                    </Tooltip>
+                                </Typography>
+                            </Box>
                         </Box>
-                    </Box>
-                </CardCover>
-            </Box>
-        </Card>
+                    </CardCover>
+                </Box>
+            </Card>
+            <Modal
+                open={open}
+                onClose={handleClose}
+            >
+                
+                <Box 
+                    sx={{ 
+                        position: 'absolute', 
+                        top: '50%', 
+                        left: '50%', 
+                        transform: 'translate(-50%, -50%)', 
+                    }}
+                >   
+                    <ModalClose
+                        variant='plain'
+                        sx={{
+                            top: 'calc(-1/4 * var(--IconButton-size))',
+                            right: 'calc(-1/4 * var(--IconButton-size))',
+                            boxShadow: '0 2px 12px 0 rgba(0 0 0 / 0.2)',
+                            borderRadius: '50%',
+                            bgcolor: 'background.surface',
+                        }}
+                    />
+                    <img 
+                        src={image} 
+                        alt={title} 
+                        style={{
+                            width: '100%',
+                            height: 'auto'
+                        }} 
+                    />
+                </Box>
+            </Modal>
+        </>
     );
 }
 
 PortfolioItem.propTypes = {
     title: PropTypes.string,
     image: PropTypes.string,
-    link: PropTypes.string,
+    repoLink: PropTypes.string,
+    webLink: PropTypes.string,
 };
 
 export default PortfolioItem;
--- src/pages/PortfolioPage.jsx
+++ src/pages/PortfolioPage.jsx
@@ -2,15 +2,21 @@ import Box from '@mui/joy/Box';
 import Typography from '@mui/joy/Typography';
 import PortfolioItem from '../components/PortfolioItem/';
 import { motion } from 'framer-motion';
+import borrowhoodImg from '../assets/borrowhood.png';
+import recipePatchImg from '../assets/recipe-patch.png';
+import pwaTextEditorImg from '../assets/pwa-text-editor.png';
+import mvcBlogImg from '../assets/mvc-blog.png';
+import jsGameImg from '../assets/js-game.png';
+import passwordGeneratorImg from '../assets/password-generator.png';
 
 export default function PortfolioPage() {
     const portfolioItems = [
-        {title: 'BorrowHood', image: '', link: 'https://polar-beach-65067-18b2f94f915d.herokuapp.com/'},
-        {title: 'Recipe Patch', image: '', link: ''},
-        {title: 'JATE: Just Another Text Editor', image: '', link: ''},
-        {title: '(MVC): Tech Blog', image: '', link: 'https://infinite-reaches-27872-0c8235452d44.herokuapp.com/'},
-        {title: 'Javascript Quiz Game', image: '', link: 'https://skywalkah.github.io/javascript-quiz-game/'},
-        {title: 'Password generator', image: '', link: 'https://skywalkah.github.io/password-generator/'}
+        {title: 'BorrowHood', image: borrowhoodImg, repoLink: 'https://github.com/skywalkah/borrowhood', webLink: 'https://polar-beach-65067-18b2f94f915d.herokuapp.com/'},
+        {title: 'Recipe Patch', image: recipePatchImg, repoLink: 'https://github.com/skywalkah/recipe-patch', webLink: 'https://skywalkah.github.io/recipe-patch/'},
+        {title: 'PWA: Text Editor', image: pwaTextEditorImg, repoLink: 'https://github.com/skywalkah/PWA-text-editor', webLink: 'https://fast-hamlet-98043-b16aa05f192d.herokuapp.com/'},
+        {title: 'MVC: Tech Blog', image: mvcBlogImg, repoLink: 'https://github.com/skywalkah/mvc-tech-blog', webLink: 'https://infinite-reaches-27872-0c8235452d44.herokuapp.com/'},
+        {title: 'Javascript Quiz Game', image: jsGameImg, repoLink: 'https://github.com/skywalkah/javascript-quiz-game', webLink: 'https://skywalkah.github.io/javascript-quiz-game/'},
+        {title: 'Password generator', image: passwordGeneratorImg, repoLink: 'https://github.com/skywalkah/password-generator', webLink: 'https://skywalkah.github.io/password-generator/'}
     ]
     return (
         <motion.div 
@@ -35,7 +41,13 @@ export default function PortfolioPage() {
                 sx={{ display: 'flex', gap: 2, flexWrap: 'wrap', p: 0, m: 0 }}
             >
                 {portfolioItems.map((portfolioItem, index) => (
-                    <PortfolioItem key={index} title={portfolioItem.title} image={portfolioItem.image} link={portfolioItem.link} />
+                    <PortfolioItem
+                        key={index}
+                        title={portfolioItem.title}
+                        image={portfolioItem.image}
+                        repoLink={portfolioItem.repoLink}
+                        webLink={portfolioItem.webLink }
+                    />
                 ))}
             </Box>
         </motion.div>
